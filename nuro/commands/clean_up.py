6d46f2ab5a6092a78199e8292858507a201210d0
Harvey Marshall <harvey.marshall@petsathome.co.uk>
2025-09-10 13:23:07 +0100

    fixing cleanup command for lists, only output lists that have been removed if they have been removed

--- nuro/commands/clean_up.py
+++ nuro/commands/clean_up.py
@@ -1,29 +1,39 @@
-import typer
 from datetime import datetime, timedelta
 from typing import List, Optional
 
-from rich.table import Table
+import typer
 from rich.console import Console
+from rich.table import Table
+from tinydb import Query
 
-from ..models.list import List as MyListModel
+from ..db.db import lists_table, tasks_table
 from ..utils.datetime_util import parse_date
-from ..db.db import tasks_table
-from ..db.db import lists_table
 
 clean_up_app = typer.Typer()
+
 
 @clean_up_app.command("lists")
 def clean_up_lists():
     lists = lists_table.all()
+    removed_lists = []
     for lst in lists:
-        tasks = tasks_table.search(lambda t: t.get("list_id") == lst["id"])
-        if not tasks:
-            lists_table.remove(doc_ids=[lst.doc_id])
-            typer.echo("✅ Removed all lists with no tasks assigned.")
+        if not lst.get("tasks", []):
+            lists_table.remove(Query().name == lst["name"])
+            removed_lists.append(lst["name"])
+    if len(removed_lists) > 0:
+        typer.echo(f"✅ Removed all lists with no tasks assigned. {removed_lists}")
+    else:
+        typer.echo("No Lists to remove; all have tasks assigned.")
+
 
 @clean_up_app.command("tasks")
 def clean_up_tasks(
-    time: Optional[int] = typer.Option(None, "--time", "-t", help="Remove tasks where due date is passed by 7 days or the value given for time")
+    time: Optional[int] = typer.Option(
+        None,
+        "--time",
+        "-t",
+        help="Remove tasks where due date is passed by 7 days or the value given for time",
+    )
 ):
     if time:
         threshold_date = datetime.now() - timedelta(days=time)
@@ -33,7 +43,7 @@ def clean_up_tasks(
             if due_date_str:
                 due_date = parse_date(due_date_str)
                 if due_date and due_date < threshold_date:
-                    tasks_table.remove(doc_ids=[task.doc_id])
+                    tasks_table.remove(Query().name == task["title"])
         typer.echo(f"✅ Removed all tasks with due dates older than {time} days.")
     else:
         threshold_date = datetime.now() - timedelta(days=7)
@@ -43,6 +53,5 @@ def clean_up_tasks(
             if due_date_str:
                 due_date = parse_date(due_date_str)
                 if due_date and due_date < threshold_date:
-                    tasks_table.remove(doc_ids=[task.doc_id])
+                    tasks_table.remove(Query().name == task["title"])
         typer.echo(f"✅ Removed all tasks with due dates older than 7 days.")
-
